5fa2b34d9235ae21c2ed7260c16c96c83e088d30
kouya17 <aoki.kouya@gmail.com>
2018-08-12 18:47:00 +0900

    add show_result()

--- red_or_white_game.py
+++ red_or_white_game.py
@@ -1,6 +1,12 @@
 from microbit import *
 import random
 
+COUNT_ONE_GAME = 5
+SCORE_RANK_S = 2000
+SCORE_RANK_A = 2500
+SCORE_RANK_B = 3000
+SCORE_RANK_C = 3500
+
 # ランダムイメージ生成関数
 # 引数    num             点灯させるLEDの数
 # 返り値   image_buffer    ランダムイメージ
@@ -34,35 +40,72 @@ def show_incorrect_image():
 def show_correct_image():
     display.show(Image.HAPPY)
 
+# カウントダウン表示関数
+def show_count_down(sec):
+    for count in range(sec, 0, -1):
+        display.show(str(count))
+        sleep(1000)
+
+# 結果表示関数
+def show_result(correct_num, total_time):
+    if correct_num < COUNT_ONE_GAME:
+        display.show("D")
+        return
+    if total_time < SCORE_RANK_S:
+        display.show("S")
+    elif total_time < SCORE_RANK_A:
+        display.show("A")
+    elif total_time < SCORE_RANK_B:
+        display.show("B")
+    elif total_time < SCORE_RANK_C:
+        display.show("C")
+    else:
+        display.show("D")
+
 # メイン処理
 uart.init(115200)
 uart.write("start.\n")
+display.show("W")
+sleep(3000)
+display.show(Image.ARROW_W)
+sleep(3000)
+display.show("R")
+sleep(3000)
+display.show(Image.ARROW_E)
+sleep(3000)
 while True:
     total_time = 0
-    random_num = random.randint(0, 24)
-    if random_num < 13:
-        correct_button = "A"
-    else:
-        correct_button = "B"
-    image = generate_ramdom_image(random_num)
-    display.show(image)
-    start_time = running_time()
-    while True:
-        if button_a.is_pressed():
-            if correct_button == "A":
-                show_correct_image()
-                break
-            else:
-                show_incorrect_image()
-                break
-        if button_b.is_pressed():
-            if correct_button == "B":
-                show_correct_image()
-                break
-            else:
-                show_incorrect_image()
-                break
-    total_time += running_time() - start_time
-    uart.write(str(total_time))
-    uart.write("\n")
-    sleep(3000)
+    correct_num = 0
+    show_count_down(3)
+    for loop_count in range(COUNT_ONE_GAME):
+        random_num = random.randint(0, 24)
+        if random_num < 13:
+            correct_button = "A"
+        else:
+            correct_button = "B"
+        image = generate_ramdom_image(random_num)
+        display.show(image)
+        start_time = running_time()
+        while True:
+            if button_a.is_pressed():
+                if correct_button == "A":
+                    show_correct_image()
+                    correct_num += 1
+                    break
+                else:
+                    show_incorrect_image()
+                    break
+            if button_b.is_pressed():
+                if correct_button == "B":
+                    show_correct_image()
+                    correct_num += 1
+                    break
+                else:
+                    show_incorrect_image()
+                    break
+        total_time += running_time() - start_time
+        uart.write(str(total_time))
+        uart.write("\n")
+        sleep(3000)
+    show_result(correct_num, total_time)
+    sleep(5000)
